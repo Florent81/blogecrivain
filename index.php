adb8b2fd7f6064f2aec332cdfccdbd3a00855e3d
Espace Pro <Espacepro@MacBook-Pro.local>
2018-09-02 21:47:53 +0200

    controllerFrontend

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
 
+require('src/controller/ControllerFrontend.php');
+
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'listPosts') {
+        listPosts();
+    }
+    elseif ($_GET['action'] == 'post') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            post();
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
+}
+else {
+    listPosts();
+}
